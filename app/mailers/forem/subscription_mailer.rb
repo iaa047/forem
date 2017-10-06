255409853feb535ca6e980660cd56fe786ed2dc2
Andrey047 <andrushka047@gmail.com>
2017-10-06 09:38:37 +0300

    fix

--- app/mailers/forem/subscription_mailer.rb
+++ app/mailers/forem/subscription_mailer.rb
@@ -4,7 +4,12 @@ module Forem
 
     def topic_reply(post_id, subscriber_id)
       # only pass id to make it easier to send emails using resque
-      @post = Post.find(post_id)
+      begin
+        @post = Post.find(post_id)
+      rescue
+        sleep 2
+        @post = Forem::Post.find(post_id)
+      end
       @user = Forem.user_class.find(subscriber_id)
 
       mail(:to => @user.email, :subject => I18n.t('forem.topic.received_reply'))
