6176ee8bb95127362a7fb8bc31d880c7179c809c
Andrey047 <andrushka047@gmail.com>
2017-10-06 12:38:24 +0300

    fix

--- app/mailers/forem/subscription_mailer.rb
+++ app/mailers/forem/subscription_mailer.rb
@@ -4,12 +4,7 @@ module Forem
 
     def topic_reply(post_id, subscriber_id)
       # only pass id to make it easier to send emails using resque
-      begin
-        @post = Post.find(post_id)
-      rescue
-        sleep 2
-        @post = Forem::Post.find(post_id)
-      end
+      @post = Post.find(post_id)
       @user = Forem.user_class.find(subscriber_id)
 
       mail(:to => @user.email, :subject => I18n.t('forem.topic.received_reply'))
--- app/models/forem/post.rb
+++ app/models/forem/post.rb
@@ -20,8 +20,8 @@ module Forem
     delegate :forum, :to => :topic
 
     after_create :set_topic_last_post_at
-    after_create :subscribe_replier
-    # after_create :skip_pending_review
+    after_commit :subscribe_replier, :if => :user_auto_subscribe?
+    after_commit :skip_pending_review
 
     class << self
       def approved
@@ -88,10 +88,9 @@ module Forem
     protected
 
     def subscribe_replier
-      if topic && user && user_auto_subscribe?
+      if topic && user
         topic.subscribe_user(user.id)
       end
-      skip_pending_review
     end
 
     # Called when a post is approved.
